8e55b658010582a0ae188ce53346c464aaa32420
KumarMohit85 <96989662+KumarMohit85@users.noreply.github.com>
2024-09-07 13:09:28 +0000

    more conversions

--- 01_Basics/03_conversionMethods.js
+++ 01_Basics/03_conversionMethods.js
@@ -24,3 +24,56 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//********************************************  Operations  *************************/
+let value= 3
+let negValue= -value
+console.log(negValue)
+
+
+let str1 = "Hello"
+let str2 = " Mohit"
+
+let str3 = str1+str2
+
+console.log(str3)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+// these are not good practices
+
+console.log(+true)
+console.log(+"")
+console.log(+"hi") // -> conversion to number using '+'
+
+let num1,num2,num3
+
+num1= num2=num3 = 2+2
+
+console.log(num1,num2)
+
+let gameCounter= 100
+++gameCounter;
+
+console.log(gameCounter)
+
+
+console.log(2<1)
+console.log("2">1)
+console.log("2"<1)
+console.log("02">1)
+
+console.log(null>=0)
+console.log(null==0) // works completely different then comparison operators.  
+//  null is only equal to undefined when using == and not equal to any other value, including 0.
+console.log(null<0)  //  When comparing null with a number, JavaScript first converts null to a number.
+
+// avoid these type of conversion
+
+
+
+
+
