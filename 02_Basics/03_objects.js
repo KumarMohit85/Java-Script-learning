34612f71e5664c68728f13483eaf6dff5a70423d
KumarMohit85 <mohit.21ug2013@iiitranchi.ac.in>
2024-09-11 19:53:07 +0530

    lesson on objects completed

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -1,7 +1,38 @@
 // singleton
 
+
 //object literals
+// Object.create()     // object creation using constructor
+
+//using symbol
+const mySym = Symbol("myKey")
 
 const JSUser = {
-
+    name: "Mohit",
+    "full name": "Mohit Kumar Pal",
+    [mySym]: "myKey1",
+    age: 21,
+    location: "Ranchi",
+    email: "mohit@gmail.com",
+    isLoggedn: false,
+    lastLoginDays: ["Monday", "Tuesday"]
 }
+
+console.log(JSUser.name)
+console.log(JSUser["name"])
+console.log(JSUser["full name"])
+console.log(JSUser[mySym])
+
+JSUser.name = "Mohit Kumar"
+
+console.log(JSUser)
+
+// Object.freeze(JSUser)     // to allow no changes
+JSUser.name = "Mohit Kumar Pal"
+console.log(JSUser)
+
+JSUser.greeting = function () {
+    console.log(`Hello JS user ${this.name}`)
+}
+
+JSUser.greeting()
